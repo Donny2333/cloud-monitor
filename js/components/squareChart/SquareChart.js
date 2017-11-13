fcba15f0d894798cc8f680787ea896c0cb413dc2
Donny <1021001390@qq.com>
2017-11-13 17:19:18 +0800

    fix screen

--- js/components/squareChart/SquareChart.js
+++ js/components/squareChart/SquareChart.js
@@ -14,6 +14,12 @@
         },
         bindToController: true,
         link: function (scope, element, attrs, ctrl) {
+          var height = $(element[0]).height();
+          var width = $(element[0]).width();
+
+          console.log(height);
+          console.log(width);
+
           var svg = d3.select(element[0]).selectAll('.squareChart'),
             g = svg.selectAll('g.bar'),
             a = parseFloat(attrs.a),
