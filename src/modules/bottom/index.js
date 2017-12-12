134b6d7ec764d203a6aad1845f21f533a492c1cd
Donny <1021001390@qq.com>
2017-12-12 14:55:08 +0800

    add ellipse chart and square chart

--- src/modules/bottom/index.js
+++ src/modules/bottom/index.js
@@ -1,5 +1,8 @@
 import angular from 'angular'
 import BottomCtrl from './BottomCtrl'
+import EllipseChart from '@/components/EllipseChart'
+import SquareChart from '@/components/SquareChart'
 
-export default angular.module('bttom', []).controller('BottomCtrl', BottomCtrl)
-  .name
+export default angular
+  .module('bttom', [EllipseChart, SquareChart])
+  .controller('BottomCtrl', BottomCtrl).name
