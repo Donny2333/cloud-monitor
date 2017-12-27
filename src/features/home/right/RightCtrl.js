036ff6af5fbfa42deb96bfc6a70d51b6de9b3d1a
Donny <1021001390@qq.com>
2017-12-27 17:07:21 +0800

    add right controller data

--- src/features/home/right/RightCtrl.js
+++ src/features/home/right/RightCtrl.js
@@ -27,6 +27,81 @@ export default class RightCtrl {
             value: 20
           }
         ]
+      },
+      {
+        label: '内存最大利用率TOP5',
+        data: [
+          {
+            name: 'DJY-A01-CMP01',
+            value: 50
+          },
+          {
+            name: 'DJY-A01-CMP02',
+            value: 42
+          },
+          {
+            name: 'DJY-A01-CMP03',
+            value: 35
+          },
+          {
+            name: 'DJY-A01-CMP04',
+            value: 28
+          },
+          {
+            name: 'DJY-A01-CMP05',
+            value: 20
+          }
+        ]
+      },
+      {
+        label: '网络流量最大利用率TOP5',
+        data: [
+          {
+            name: 'DJY-A01-CMP01',
+            value: 50
+          },
+          {
+            name: 'DJY-A01-CMP02',
+            value: 42
+          },
+          {
+            name: 'DJY-A01-CMP03',
+            value: 35
+          },
+          {
+            name: 'DJY-A01-CMP04',
+            value: 28
+          },
+          {
+            name: 'DJY-A01-CMP05',
+            value: 20
+          }
+        ]
+      },
+      {
+        label: '系统负载最大利用率TOP5',
+        data: [
+          {
+            name: 'DJY-A01-CMP01',
+            value: 50
+          },
+          {
+            name: 'DJY-A01-CMP02',
+            value: 42
+          },
+          {
+            name: 'DJY-A01-CMP03',
+            value: 35
+          },
+          {
+            name: 'DJY-A01-CMP04',
+            value: 28
+          },
+          {
+            name: 'DJY-A01-CMP05',
+            value: 20
+          }
+        ]
       }
     ]
   }
