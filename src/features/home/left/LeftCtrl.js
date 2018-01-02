6a5f6ec2fb7579016417a987f69b29341b947c9c
Donny <1021001390@qq.com>
2018-01-02 17:17:34 +0800

    add circle chart

--- src/features/home/left/LeftCtrl.js
+++ src/features/home/left/LeftCtrl.js
@@ -2,9 +2,47 @@ export default class LeftCtrl {
   constructor() {
     let that = this
 
-    that.counts_of_host = {
+    that.countsOfHost = {
       label: '物理云主机数',
       value: 124
     }
+
+    that.usageList = [{
+      title: 'CPU资源使用率',
+      color: '#0098ff',
+      detail: {
+        totalName: '总数',
+        totalValue: 100,
+        totalUnit: '核',
+        usageName: '已使用',
+        usageValue: 30,
+        usageUnit: '核',
+        percent: '0'
+      }
+    }, {
+      title: '内存资源使用率',
+      color: '#58c84d',
+      detail: {
+        totalName: '总数',
+        totalValue: 100,
+        totalUnit: 'GB',
+        usageName: '已分配',
+        usageValue: 20,
+        usageUnit: 'GB',
+        percent: '0'
+      }
+    }, {
+      title: '存储资源使用率',
+      color: '#ff9b0a',
+      detail: {
+        totalName: '总数',
+        totalValue: 100,
+        totalUnit: 'GB',
+        usageName: '已分配',
+        usageValue: 40,
+        usageUnit: 'GB',
+        percent: '0'
+      }
+    }]
   }
 }
--- src/features/home/left/index.js
+++ src/features/home/left/index.js
@@ -2,7 +2,8 @@ import angular from 'angular'
 import LeftCtrl from './LeftCtrl'
 import countBoard from '@/components/countBoard'
 import dashBoard from '@/components/dashBoard'
+import circleChart from '@/components/circleChart'
 
-export default angular.module('left', [countBoard, dashBoard])
+export default angular.module('left', [countBoard, dashBoard, circleChart])
   .controller('LeftCtrl', LeftCtrl)
   .name
