5f60ac931c06ffc28b5f3468a00725a3ed1ccd6c
Donny <1021001390@qq.com>
2018-01-10 09:05:06 +0800

    add monitor service

--- src/services/Monitor/Monitor.js
+++ src/services/Monitor/Monitor.js
@@ -3,8 +3,14 @@ import { URL_CFG } from '@/api'
 export default class Monitor {
   constructor(Http) {
     return {
+      detail: () => {
+        return Http.get(URL_CFG.api + 'detail')
+      },
       rs_statics: () => {
         return Http.get(URL_CFG.api + 'rs_statics')
+      },
+      topN: params => {
+        return Http.get(URL_CFG.api + `topN/${params.metric}/${params.num}`)
       }
     }
   }
